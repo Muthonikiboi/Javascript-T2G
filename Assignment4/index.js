3b8315013a42c8338e1a4f322628c1396c7db6f1
Muthonikiboi <kiboijoye254@gmail.com>
2024-09-13 08:11:46 +0300

    ADD:Changes

--- Assignment4/index.js
+++ Assignment4/index.js
@@ -1,3 +1,13 @@
-fetch("https://run.mocky.io/v3/aae3903b-64bf-4285-920d-0be7f5345e76")
-.then(res => res.json())
-.then(data =>console.log(data));
+const product = async function(){
+    const url = 'https://run.mocky.io/v3/aae3903b-64bf-4285-920d-0be7f5345e76'
+
+    try{
+        const result = await fecth (url)
+        const data = result.json()
+        console.log(data)
+    }catch(e){
+        console.error('Error:', e)
+    }
+
+}
+product()
